272bb1d501ace99b119813c2ee636d381b97f175
garrettgu10 <garrettgu777@gmail.com>
2016-10-19 16:23:56 -0500

    added test correspondence

--- js/init.js
+++ js/init.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   $('select').material_select();
 });
+
+function init(){
+  $("#science_hours").change(function(){
+    $("#entertainment_hours").val(24-$("#science_hours").val());
+  });
+}
