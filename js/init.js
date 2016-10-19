935c05fb9ee92f91dc050e57114817e7356dddf4
garrettgu10 <garrettgu777@gmail.com>
2016-10-19 17:23:58 -0500

    new logo, added slider text

--- js/init.js
+++ js/init.js
@@ -5,5 +5,6 @@ $(document).ready(function() {
 function init(){
   $("#science_hours").change(function(){
     $("#entertainment_hours").val(24-$("#science_hours").val());
+    $("#science_hours_label").html($("#science_hours").val());
   });
 }
