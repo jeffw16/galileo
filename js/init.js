93c2411f3b699b122aa810b44a3d66abcf4d076d
garrettgu10 <garrettgu777@gmail.com>
2016-10-19 21:56:03 -0500

    Added decision timer

--- js/init.js
+++ js/init.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
   $('select').material_select();
 });
 
+decisiontime = 0;
+
 function init(){
   $("#science_hours").change(function(){
     $("#entertainment_hours").val(24-$("#science_hours").val());
     $("#science_hours_label").html($("#science_hours").val());
+    $("#eating_hours").val(3);
   });
+
+  setInterval(function(){
+    decisiontime++;
+    $("#decision_hours").css("width", decisiontime/60*100+"%");
+    if(decisiontime >= 30){
+      decisiontime = 0;
+    }
+  },1000)
 }
