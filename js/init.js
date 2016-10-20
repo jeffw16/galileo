d9a2a38e264688161b7a5214d9f6592f4c375352
garrettgu10 <garrettgu777@gmail.com>
2016-10-19 23:32:11 -0500

    Added hour counters

--- js/init.js
+++ js/init.js
@@ -1,21 +1,48 @@
 $(document).ready(function() {
   $('select').material_select();
+  init();
 });
 
 decisiontime = 0;
 
+decisionHours = 0;
+
+function setEatingHours(val){
+  $("#eating_hours").val(val);
+  $("#eating_hours").material_select();
+}
+
+function setEatingHoursMax(val){
+  for(var i = 1; i <= 3; i++){
+    if(i <= val){
+      $("#eating_hours option[value='"+i+"']").prop("disabled",false);
+    }else{
+      $("#eating_hours option[value='"+i+"']").prop("disabled",true);
+    }
+  }
+  $("#eating_hours").material_select();
+}
+
 function init(){
   $("#science_hours").change(function(){
     $("#entertainment_hours").val(24-$("#science_hours").val());
+    $("#entertainment_hours_label").html($("#entertainment_hours").val());
     $("#science_hours_label").html($("#science_hours").val());
-    $("#eating_hours").val(3);
+  });
+
+  $("#entertainment_hours").change(function(){
+    $("#science_hours").val(24-$("#entertainment_hours").val());
+    $("#science_hours_label").html($("#science_hours").val());
+    $("#entertainment_hours_label").html($("#entertainment_hours").val());
   });
 
   setInterval(function(){
     decisiontime++;
-    $("#decision_hours").css("width", decisiontime/30*100+"%");
-    if(decisiontime >= 30){
+    $("#decision_hours").css("width", decisiontime/60*100+"%");
+    if(decisiontime >= 60){
       decisiontime = 0;
+      decisionHours++;
+      $("#decision_hours_label").html(decisionHours);
     }
   },1000)
 }
